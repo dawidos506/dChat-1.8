e4366d74b360429a8ef1981aab881b625c27ca70
dawidos506 <dawidv17@wp.pl>
2018-07-27 11:12:43 +0200

    ChatCmd initial

---
+++ src/dChat/cmds/ChatCmd.java
@@ -0,0 +1,17 @@
+package dChat.cmds;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class ChatCmd implements CommandExecutor {
+	
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		if(cmd.getName().equalsIgnoreCase("name")) {
+			
+		}
+		return false;
+	}
+
+}
